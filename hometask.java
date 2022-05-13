86923856727bde41c80932fdc1e14b690b0d373e
DmitryLihtorovich <70798585+DmitryLihtorovich@users.noreply.github.com>
2022-05-13 14:51:12 +0300

    Add files via upload

---
+++ hometask.java
@@ -0,0 +1,50 @@
+package Hometask;
+
+import java.util.Scanner;
+
+public class hometask {
+    public static void main(String[] args) {
+        //1. Составьте программу, выводящую на экран квадраты чисел от 10 до 20
+        /*Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for (int i = a; i < 20; i++) {
+            System.out.println(Math.pow(i, 2));
+        }
+                 */
+        //2.Составьте программу, котораЯ вычисляет сумму чисел от 1 до n.значение n вводится с клавиатуры.
+        /*Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i+i);
+        }
+                 */
+        //3. Составьте программу, составьте программу, которая вычисляет произведение чисел от 1 до n.n вводится с клавиатуры.
+        /*Scanner in = new Scanner(System.in);
+        double n = in.nextDouble();
+        for (double i = 1.0; i <= n; i++) {
+            System.out.println(i*i);
+        }
+                 */
+        //4. С клавиатуры вводятся n чисел. Составьте программу, которая определяет кол-во отрицательных, кол-во положительных и кол-во нулей среди введеных чисел.
+        // Значение n вводится с клавиатуры.
+        /*Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int z1 = 0;
+        int z2 = 0;
+        int z3 = 0;
+        if (n > 0) {
+            for (int i = n; i > 0; i++);
+            z1 = n;
+        }
+        if (n < 0) {
+            for (int i = n; i < 0; i++) ;
+            z2 =Math.abs(n);
+        }
+        if (n == 0) {
+            for (int i = n; i == 0; i++) ;
+            z3++;
+        }
+        System.out.println("polozh= " + z1 + " otr= " + z2 + " null= " + z3);
+                 */
+    }
+}
